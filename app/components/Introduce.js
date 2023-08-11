cda805cf2bc99a2a1b4ef001a22b536d55e2d51a
2inlee <kbs@kakao.com>
2023-08-11 20:10:30 +0900

    edit Introduce

--- app/components/Introduce.js
+++ app/components/Introduce.js
@@ -5,7 +5,7 @@ const Intorduce = () => {
     <div>
       <div className="container text-center text-4xl mx-auto max-w-[80%] font-bold">
         <p>Hi I`m Inho Lee</p>
-        <p>They are developers who use every method they think is efficient to solve problems.</p>
+        <p>I'm a developer who uses any way to solve problems</p>
       </div>
       <div className="mt-20" />
     </div>
---
+++ app/components/Project.js
@@ -0,0 +1,40 @@
+'use client';
+import HorizonLine from "../utils/HorizonLine";
+
+const Project = () => {
+  return (
+      <div className="container text-left text-4xl mx-auto max-w-[80%] font-extrabold">
+      <div className="mt-40" />
+        <p>About</p>
+        <HorizonLine text="Me"/>
+      <br />
+      <div className="font-bold">
+        <p>Poem</p>
+      </div>
+      <br />
+      <div className="container text-left text-xl font-medium">
+        <p>I am a computer software engineering student who loves literature more than anyone else.
+          <br />
+          <br />
+          I like to read books and write.
+          <br />
+          But I found that developing is not much different from reading books and writing.
+          <br />
+          <br />
+          Some people say that development is hard and mechanical
+          I think the process of coding line by line is no different from writing, drawing, and speaking logically.
+          <br />
+          You think more to write better poems, and you think more to write better codes like you're refining the context over and over again.
+          <br />
+          <br />
+          I think the only difference between art and development is the speed of how quickly it can change the world.
+          <br />
+          <br />
+          I am an artist who wants to change into a better world.</p>
+      </div>
+      <br />
+      </div>
+  );
+};
+
+export default Project;
